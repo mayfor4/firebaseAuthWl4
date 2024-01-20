5e2be57633dcfdc9b2fcbe1ffd87c7f584baf6ae
chenaoh <mayfor4>
2024-01-19 22:14:57 -0600

    ya funciona

--- js/app.js
+++ js/app.js
@@ -1,14 +1,37 @@
+
+
+// Importar el módulo de autenticación de Firebase
+const auth = firebase.auth();
+
+// Función para iniciar sesión
 function loginUser() {
   var email = document.getElementById("correo").value;
-  var password = document.getElementById("contraseña").value;
+  var password = document.getElementById("contrasena").value;
 
-  console.log("Correo: " + email + ", Contraseña: " + password);
+  console.log(`correo: ${email}, contraseña: ${password}`);
 
-  firebase.auth().signInWithEmailAndPassword(email, password)
+  auth.signInWithEmailAndPassword(email, password)
     .then((success) => {
-      console.log('Inicio de sesión exitoso: ', success);
+      console.log("Inicio de sesión exitoso");
     })
     .catch((error) => {
-      console.error('Error en el inicio de sesión: ', error);
+      console.error("Error al iniciar sesión", error.message);
     });
 }
+
+// Función para cerrar sesión
+function logoutUser() {
+  firebase.auth().signOut()
+    .then(() => {
+      // Se cerró sesión exitosamente
+      alert('Cerrar sesión')
+      console.log("Cierre de sesión exitoso");
+      window.location.replace("/index.html");
+    })
+    .catch((error) => {
+      // Si hay un error, manejarlo adecuadamente
+      console.error("Error al cerrar sesión", error.message);
+      // Puedes mostrar un mensaje de error al usuario aquí
+    });
+}
+
